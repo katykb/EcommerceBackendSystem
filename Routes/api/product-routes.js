94d8118f39d80221ff4c4888778357dd9c9e5398
Katy Brown <katybr35@gmail.com>
2022-06-14 10:47:20 -0500

    reorg files

--- Routes/api/product-routes.js
+++ Routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../Seeds');
 
 // The `/api/products` endpoint
 
